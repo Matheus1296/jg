c747ce25b2a781b9c6013bc0598fccc3eb00a9d0
matheus1296 <matheuspferreira96@gmail.com>
2024-11-05 17:00:14 -0300

    feat: implements neo4j connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
 
 const typeDefs = `#graphql
-  type Query {
-    test: String
-  }
+    type Movie @node {
+        title: String
+        actors: [Person!]! @relationship(type: "ACTED_IN", direction: IN)
+    }
+
+    type Person @node {
+        name: String
+        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+    }
 `;
+
 async function startServer() {
+  const driver = neo4j.driver(
+    "bolt://34.201.6.178",
+    neo4j.auth.basic("neo4j", "fist-varactors-mineral")
+  );
+
+  const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+  const schema = await neoSchema.getSchema();
+
   const server = new ApolloServer({
-    typeDefs,
-    resolvers: {
-      Query: {
-        test: () => "Hello World!",
-      },
-    },
+    schema,
   });
 
   const { url } = await startStandaloneServer(server, {
